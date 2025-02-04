d8a9fedf54f175ee6b6354315c8e648167e743ea
David Vanderklay <davidvanderklay@gmail.com>
2025-02-03 21:51:23 -0600

    associations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,7 @@
 class Book < ApplicationRecord
+  has_many :users, through: :user_books
+  has_many :user_books
+
   validates :title, presence: { message: 'must be provided' }
   validates :author, presence: { message: 'must be provided' }
   validates :price, presence: { message: 'must be provided' }, numericality: { greater_than_or_equal_to: 0 }
